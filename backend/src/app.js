00be6c3886cbe244a5452c0f2ac65e8cf08cd65a
SaadSyed <saadali7142@gmail.com>
2024-05-20 01:31:59 +0500

    base get path added

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
-
+app.get("/", (req, res) => res.send("Express on Vercel"));
 const start = async () => {
   await connectDB(config.MONGODB_URL);
   app.listen(config.PORT || 3000, () => {
