046f9d2ede4dfd35bb019941b10cbdb33b89f815
SaadSyed <saadali7142@gmail.com>
2024-05-20 01:12:14 +0500

    controller updated

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,5 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import PostModel from "../models/post.model.js";
+import NotFoundError from "../errors/not-found.js";
+import BadRequestError from "../errors/bad-request.js";
 
 export const createPost = async (req, res) => {
   const userId = req.user._id;
@@ -17,11 +19,67 @@ export const createPost = async (req, res) => {
   });
 };
 
-export const getPosts = async (req, res) => {
-  const posts = await PostModel.find({}).populate("userId", "name email");
+export const getPostsByUser = async (req, res) => {
+  const posts = await PostModel.find({ userId: req.user._id }).populate(
+    "userId",
+    "name email"
+  );
 
   res.status(StatusCodes.OK).json({
     success: true,
     data: posts,
+  });
+};
+
+export const getPost = async (req, res) => {
+  const post = await PostModel.findById(req.params.id);
+  if (!post) throw new NotFoundError("Post not found!");
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    data: post,
+  });
+};
+
+export const updatePost = async (req, res) => {
+  const post = await PostModel.findById(req.params.id);
+  if (!post) throw new NotFoundError("Post not found");
+
+  if (post.userId.toString() !== req.user._id)
+    throw new BadRequestError(
+      "You can't edit this post because you're not the owner of this post!"
+    );
+
+  const updatePost = await PostModel.findByIdAndUpdate(
+    post._id,
+    {
+      ...req.body,
+    },
+    {
+      runValidators: true,
+      new: true,
+    }
+  );
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    data: updatePost,
+  });
+};
+
+export const deletePost = async (req, res) => {
+  const post = await PostModel.findById(req.params.id);
+  if (!post) throw new NotFoundError("Post not found");
+
+  if (post.userId.toString() !== req.user._id)
+    throw new BadRequestError(
+      "You can't edit this post because you're not the owner of this post!"
+    );
+
+  const deletePost = await PostModel.findByIdAndDelete(post._id);
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    message: "Post deleted successfully!",
   });
 };
--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { createPost, getPosts } from "../controllers/post.controller.js";
+import {
+  createPost,
+  deletePost,
+  getPost,
+  getPostsByUser,
+  updatePost,
+} from "../controllers/post.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const postRouter = express.Router();
 
 postRouter.post("/", isAuthenticated, createPost);
-postRouter.get("/", getPosts);
+postRouter.get("/", isAuthenticated, getPostsByUser);
+postRouter.get("/:id", getPost);
+postRouter.patch("/:id", isAuthenticated, updatePost);
+postRouter.delete("/:id", isAuthenticated, deletePost);
 
 export default postRouter;
